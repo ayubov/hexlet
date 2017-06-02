0e6fddf91102d1709667427fd074d6ce2967e819
ayubov <alvi.ayubov@yandex.ru>
2017-06-02 16:23:23 +0300

    folder is deleted

--- sicp/chapter-1/ex-1-06.scm
+++
@@ -1,1 +0,0 @@
-; The function will never terminate, because new-if is a function and it's arguments will be evaluated before the function is called.  One of the arguments is a recursive call.
--- sicp/ex-1-04.scm
+++
@@ -1,4 +0,0 @@
-(define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
-
-  ;If predicate (> b 0) is true, it would be (+ a b), else (- a b) 
