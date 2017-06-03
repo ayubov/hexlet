d12f8cb16eade98e0fb858e50845ef8a802e9973
ayubov <alvi.ayubov@yandex.ru>
2017-06-03 10:48:39 +0300

    folder is deleted

--- sicp/chapter-1/ex-1-03.scm
+++
@@ -1,7 +0,0 @@
-(def (sum-of-larger-square a b c)
-     (cond
-           ((and (a > c) (b > c)) (+ (* b b) (* a a)))
-           ((and (a > b) (c > b)) (+ (* a a) (* c c)))
-           ((and (b > a) (c > a)) (+ (* b b) (* c c)))
-     ))
-           
